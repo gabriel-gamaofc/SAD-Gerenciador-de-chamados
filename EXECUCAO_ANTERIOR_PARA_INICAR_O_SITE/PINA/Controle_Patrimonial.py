0c6f87debfe6227c02ff2eab24586a8f7a72d881
Gabriel Gama <130513430+gabriel-gamaofc@users.noreply.github.com>
2024-03-26 14:54:33 -0300

    Limpeza

--- EXECUCAO_ANTERIOR_PARA_INICAR_O_SITE/PINA/Controle_Patrimonial.py
+++ EXECUCAO_ANTERIOR_PARA_INICAR_O_SITE/PINA/Controle_Patrimonial.py
@@ -20,10 +20,10 @@ def get_responses_from_sheet(sheet_url):
 
         # Inicializar a conexão com o banco de dados
         connection = mysql.connector.connect(
-           host='localhost',
-           user='dba',
-           password='Sad#Suporte',
-           database='expresso_conectado'
+           host='',
+           user='',
+           password='',
+           database=''
         )
         
         # Criar o cursor
@@ -76,5 +76,5 @@ def get_responses_from_sheet(sheet_url):
         print("Falha ao obter respostas da planilha.")
 
 # Exemplo de uso
-sheet_url = "https://docs.google.com/spreadsheets/d/1GChEXYjREHpozDMpH1jivxrSXgRRTyhbPf7LTzy3n8Y/edit?usp=sharing"
+sheet_url = ""
 get_responses_from_sheet(sheet_url)
